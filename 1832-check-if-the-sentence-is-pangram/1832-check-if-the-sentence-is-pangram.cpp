c4b8be94996be604ccb8d0d143632d2e01cca571
zaureqs <80621268+zaureqs@users.noreply.github.com>
2022-08-10 00:22:30 +0530

    Time: 8 ms (20.29%), Space: 6.7 MB (27.51%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool checkIfPangram(string s) {
         set<char> a;
-        
+        if(s.size()<26)
+            return false;
         for(int i = 0 ;i<s.size();i++){
             a.insert(s[i]);
         }
