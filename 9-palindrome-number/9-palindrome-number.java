75167e6d5b02ae2cce3bd1b0c64ab899b74e84d3
zaureqs <80621268+zaureqs@users.noreply.github.com>
2022-08-27 02:11:59 +0530

    Time: 18 ms (29.31%), Space: 45.2 MB (33.55%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,9 +1,9 @@
 class Solution {
-    public boolean isPalindrome(int x) {
+    public boolean isPalindrome(int x){
         String str = String.valueOf(x);
         int l = 0;
         int r = str.length()-1;
-        while(l<=r){
+        while(l<r){
             if(str.charAt(l)!=str.charAt(r))
                return false;
             l++;
