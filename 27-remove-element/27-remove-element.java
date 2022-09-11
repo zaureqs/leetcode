e420de4a05a9646fb316401cc8dc2401971931dd
zaureqs <80621268+zaureqs@users.noreply.github.com>
2022-09-11 16:27:18 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (34.92%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -1,11 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int l = 0, r = nums.length-1;
-        if(nums.length==1){
-            if(nums[0]==val)
-                return 0;
-            return 1;
-        }
         while(l<=r){
             if(nums[r]==val){
                 r--;
