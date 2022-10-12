933dc8c7e98661aa26771ae9be37feab9b1987e0
zaureqs <80621268+zaureqs@users.noreply.github.com>
2022-10-13 00:40:29 +0530

    Time: 17 ms (38.07%), Space: 5.9 MB (38.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        int ans = 0;
-        int a = 1,b = 0;
-        int c = 1;
-        for(int i=1;i<=n;i++)
-        {
-            c = a+b;
-            a=b;
-            b=c;
-    
-            ans = c;
-        }
-
-        return ans;
+        if(n<=1)
+            return n;
+        
+        else
+            return fib(n - 1) + fib(n - 2);
     }
 };
