64aad9250e55d8ac4ca371dfae73269fdede9296
zaureqs <80621268+zaureqs@users.noreply.github.com>
2022-11-24 22:04:51 +0530

    Added solution - LeetHub

---
+++ Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        vector<int> Union;
+        int i = 0;
+        int j = 0;
+        while(i < n && j < m)
+        {
+            if(arr1[i] == arr2[j])
+            {
+                if(Union.size() == 0 || arr1[i] != Union.back())
+                    Union.push_back(arr1[i]); 
+                
+                i++; j++;
+            }
+            else if(arr1[i] < arr2[j])
+            {
+                if(Union.size() == 0 || arr1[i] != Union.back())
+                    Union.push_back(arr1[i]);
+                i++;
+                
+            }
+            else
+            {
+                if(Union.size() == 0 || arr2[j] != Union.back())
+                    Union.push_back(arr2[j]);
+                j++;
+            }
+        }
+        while(i < n)
+        {
+            if(Union.size() == 0 || arr1[i] != Union.back())
+                Union.push_back(arr1[i]);
+            i++;
+        }
+        while(j < m)
+        {
+            if(Union.size() == 0 || arr2[j] != Union.back())
+                Union.push_back(arr2[j]);
+            j++;
+        }
+        return Union;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    
+	    
+	    
+	    int N, M;
+	    cin >>N >> M;
+	    
+	    int arr1[N];
+	    int arr2[M];
+	    
+	    for(int i = 0;i<N;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<M;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findUnion(arr1,arr2, N, M);
+	    for(int i: ans)cout<<i<<' ';
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
